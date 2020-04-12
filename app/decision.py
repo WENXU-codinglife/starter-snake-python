61c102bd7c687b158b384ca740b953f545cbc8db
WENXU-codinglife <xu_wen@sfu.ca>
2020-04-11 21:56:32 -0700

    direction

--- app/decision.py
+++ app/decision.py
@@ -4,43 +4,42 @@ def dec(data):
     height = data['board']['height']
     width = data['board']['width']
     arena = layout(height,width,data)
-	print(arena)
     first = data['you']['body'][0]
     second = data['you']['body'][1]
     direction = 'up'
     if first['x'] == second['x']:
     	if first['y'] > second['y']:
     		# downward
-    		if arena['snake'][first['x']+1][first['y']+2] == 0:
+    		if arena['snakes'][first['x']+1][first['y']+2] == 0:
     			direction = 'down'
-    		elif arena['snake'][first['x']][first['y']+1] == 0:
+    		elif arena['snakes'][first['x']][first['y']+1] == 0:
     			direction = 'left'
-    		elif arena['snake'][first['x'+2]][first['y']+1] == 0:
+    		elif arena['snakes'][first['x']+2][first['y']+1] == 0:
     			direction = 'right'
     	else:
     		#upward
-    		if arena['snake'][first['x']+1][first['y']] == 0:
+    		if arena['snakes'][first['x']+1][first['y']] == 0:
     			direction = 'up'
-    		elif arena['snake'][first['x']][first['y']+1] == 0:
+    		elif arena['snakes'][first['x']][first['y']+1] == 0:
     			direction = 'left'
-    		elif arena['snake'][first['x'+2]][first['y']+1] == 0:
+    		elif arena['snakes'][first['x']+2][first['y']+1] == 0:
     			direction = 'right' 		
     elif first['y'] == second['y']:
     	if first['x'] > second['x']:
     		#rightward
-    		if arena['snake'][first['x']+2][first['y']+1] == 0:
+    		if arena['snakes'][first['x']+2][first['y']+1] == 0:
     			direction = 'right'
-    		elif arena['snake'][first['x']+1][first['y']] == 0:
+    		elif arena['snakes'][first['x']+1][first['y']] == 0:
     			direction = 'up'
-    		elif arena['snake'][first['x'+1]][first['y']+2] == 0:
+    		elif arena['snakes'][first['x']+1][first['y']+2] == 0:
     			direction = 'down'		
     	else:
     		#leftward
-    		if arena['snake'][first['x']][first['y']+1] == 0:
+    		if arena['snakes'][first['x']][first['y']+1] == 0:
     			direction = 'left'
-    		elif arena['snake'][first['x']+1][first['y']] == 0:
+    		elif arena['snakes'][first['x']+1][first['y']] == 0:
     			direction = 'up'
-    		elif arena['snake'][first['x'+1]][first['y']+2] == 0:
+    		elif arena['snakes'][first['x']+1][first['y']+2] == 0:
     			direction = 'down'	
     return direction
 
@@ -48,14 +47,14 @@ def dec(data):
 
 def  layout(height,width,data):
 	b = [0]*(width+2)
-	snake = [b] * (height+2)
+	snakes = [b] * (height+2)
 	food = [b] * (height+2)
 	row = 0
 	column = 0
 	while(row < height+2):
 		while(column < width+2):
 			if(row == 0 or row == height+1 or column == 0 or column == width+1):
-				snake[row][column] = 1
+				snakes[row][column] = 1
 			column = column + 1
 		row = row + 1
 
@@ -64,13 +63,13 @@ def  layout(height,width,data):
 		food[piece['x']+1][piece['y']+1] = 1
 
 	for component in data['you']['body']:
-		snake[component['x']+1][component['y']+1] = 1
+		snakes[component['x']+1][component['y']+1] = 1
 
-	for players in data['board']['snake']:
-		for player in players:
-			for component in player['body']:
-				snake[component['x']+1][component['y']+1] = 1
-	ret = {"snake":snake, "food":food}
+	for players in data['board']['snakes']:
+		print(players)
+		for component in players['body']:
+			snakes[component['x']+1][component['y']+1] = 1
+	ret = {"snakes":snakes, "food":food}
 	return ret
 
 
---
+++ app/t.py
@@ -0,0 +1,45 @@
+from decision import dec
+
+data = {
+    "turn": 5, 
+    "game": {"id": "34e3647b-5798-4df4-a376-a53f78306e44"}, 
+    "board": {
+        "width": 11, 
+        "snakes": [
+            {"id": "gs_pM7yYCK8KKbrPjVVkSdbkXqC", 
+            "name": "johnwilkes-0.7", 
+            "body": [
+                {"x": 1, "y": 0}, 
+                {"x": 1, "y": 1}, 
+                {"x": 1, "y": 2}
+                ], 
+            "shout": "", 
+            "health": 95
+            }, 
+            {"id": "gs_TFx4JR3wcPXDTjKpDGQH9YGS", 
+            "name": "drasnakegon", 
+            "body": [
+                {"x": 5, "y": 4}, 
+                {"x": 5, "y": 5}, 
+                {"x": 5, "y": 6}
+                ], 
+            "shout": "", 
+            "health": 95
+            }
+        ], 
+        "height": 11, 
+        "food": [{"x": 8, "y": 10}, {"x": 7, "y": 8}, {"x": 2, "y": 3}]
+    }, 
+    "you": {
+        "id": "gs_TFx4JR3wcPXDTjKpDGQH9YGS", 
+        "name": "drasnakegon", 
+        "body": [
+            {"x": 5, "y": 4}, 
+            {"x": 5, "y": 5}, 
+            {"x": 5, "y": 6}], 
+            "shout": "", 
+            "health": 95
+        }
+    }
+
+print(dec(data))
